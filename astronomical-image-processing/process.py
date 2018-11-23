ca653670c97be1305a241d9a8cf277b64ff71750
ricktjwong <rickwtj@gmail.com>
2018-11-23 23:46:08 +0000

    Attempt gaussian fit on a row of data

--- astronomical-image-processing/process.py
+++ astronomical-image-processing/process.py
@@ -1,6 +1,10 @@
+# rtw16 and drs16
+# v1.0
+
 from astropy.io import fits
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.signal import argrelextrema
 
 hdulist = fits.open("../data/mosaic.fits")
 data = hdulist[0].data
@@ -10,6 +14,15 @@ print (type(data))
 print (len(data))
 print (data[0])
 
+def G(x, mu, sigma):
+    print(mu)
+    print(sigma)
+    return (1/(2*np.pi)**0.5*sigma) * np.exp(-((x - mu)**2)/(2*sigma))
+
+#def sigma(G):
+#    mu = np.mean(G)
+#    return (1/len(G) * sum([(i - mu)**2 for i in G])) ** 0.5
+
 def get_brightest(nparray):
     max = 0
     for i in nparray:
@@ -17,7 +30,42 @@ def get_brightest(nparray):
             max = i
     return max
 
+def smooth(y, box_pts):
+    box = np.ones(box_pts) / box_pts
+    y_smooth = np.convolve(y, box, mode='same')
+    return y_smooth
+
 x = np.array([[1,1,1,1,1], [1,1,1,1,2]])
 
-for i in range(200):
-    print(get_brightest(data[i]))
+#for i in range(200):
+#    print(get_brightest(data[i]))
+#    plt.hist(i, 20)
+    
+data_flatten = data.flatten()
+
+#plt.hist(data_flatten)
+
+one_row = data[3211]
+smoothened_row = smooth(one_row, 10)
+x_array = [i for i in range(len(data[0]))]
+#plt.plot(x_array, one_row)
+#plt.plot(x_array, smooth(one_row, 1))
+#plt.plot(x_array, smooth(one_row, 5))
+#plt.plot(x_array, smoothened_row)
+
+extremas = argrelextrema(smoothened_row, np.greater, order=10)
+
+plt.figure(2)
+gaussian = one_row[2459 - 50 : 2459 + 50]
+offset_gaussian = [i - min(gaussian) for i in gaussian]
+candidate_gaussian = smooth(offset_gaussian, 10)
+sigma = np.std(candidate_gaussian)
+x = np.linspace(2459 - 50, 2459 + 50, 100)
+gaussian_fit = G(x, 2459, sigma)
+
+area1 = np.trapz(candidate_gaussian)
+area2 = np.trapz(gaussian_fit)
+gaussian_fit2 = G(x, 2459, sigma) * area1/area2
+
+plt.plot(x, candidate_gaussian)
+plt.plot(x, gaussian_fit2)
